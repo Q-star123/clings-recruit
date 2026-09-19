bf4e48fb772d449c9929f4778e124be1a976f001
Q-star123 <2686934842@qq.com>
2026-09-19 19:25:22 +0800

    Implement even/odd check in judge.c

--- exercises/04_odd_or_even/judge.c
+++ exercises/04_odd_or_even/judge.c
@@ -22,8 +22,13 @@ int main(void) {
     int num;
 
     scanf("%d", &num);
+if(num%2==0){
+		printf("number %d is even\n",num);
+	}
+	else{
+		printf("number %d is odd\n",num);
+	}
 
-#error TODO: Use if/else to check num%2==0, print "num %d is even/odd\n". Run "clings hint" for help.
 
     return 0;
 }
